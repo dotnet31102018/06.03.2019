892c4414c800fd6b02b17e46158fe733ffcfd671
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-03-06 21:15:07 +0200

    Add files via upload

---
+++ 0603DictSolution/0603DictSolution/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0603DictSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string result = string.Empty;
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "one");
+            dictionary.Add(2, "Two");
+            dictionary.Add(3, "Three");
+            dictionary.Add(4, "Four");
+            dictionary.Add(5, "Five");
+            dictionary.Add(6, "Six");
+            dictionary.Add(7, "Seven");
+            dictionary.Add(8, "Eight");
+            dictionary.Add(9, "Nine");
+            dictionary.Add(-1, "Exit");
+
+            do
+            {
+                Console.Write("Enter Number :");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (dictionary.TryGetValue(number, out result))
+                {
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Not Found!");
+                }
+            }
+            while (result != "Exit");
+
+        }
+    }
+}
